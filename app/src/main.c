93d4865531ddf5dfda22680a84c1400e1f72166a
Daniel R. Slater <slaterd314@gmail.com>
2023-06-01 17:22:41 -0400

    WIP:STASH

--- app/src/main.c
+++ app/src/main.c
@@ -6,12 +6,102 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/sensor.h>
 #include <app_version.h>
-
 #include <zephyr/logging/log.h>
+#include "rpmsg_lite.h"
+#include "rpmsg_queue.h"
+#include "rpmsg_ns.h"
+#include "rpmsg_config.h"
+#include <assert.h>
+#include "board.h"
+
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define RPMSG_LITE_SHMEM_BASE         (VDEV0_VRING_BASE)
+#define RPMSG_LITE_LINK_ID            (RL_PLATFORM_IMX8MM_M4_USER_LINK_ID)
+#define RPMSG_LITE_NS_ANNOUNCE_STRING "rpmsg-virtual-tty-channel-1"
+#define APP_TASK_STACK_SIZE (256)
+#ifndef LOCAL_EPT_ADDR
+#define LOCAL_EPT_ADDR (30)
+#endif
+
+#if 0
+/* Globals */
+static char app_buf[512]; /* Each RPMSG buffer can carry less than 512 payload */
+
+static struct rpmsg_lite_instance *volatile my_rpmsg = NULL;
+
+static struct rpmsg_lite_endpoint *volatile my_ept = NULL;
+static volatile rpmsg_queue_handle my_queue        = NULL;
+#endif // 0
 
 int main(void)
 {
+#if 0	
+    volatile uint32_t remote_addr;
+    void *rx_buf;
+    uint32_t len;
+    int32_t result;
+    void *tx_buf;
+    uint32_t size;
+#endif // 0
+	    /* Print the initial banner */
+    printk("\r\nRPMSG String Echo FreeRTOS RTOS API Demo...\r\n");
+
+	k_sleep(K_MSEC(1000));
+#if 0
+	my_rpmsg = rpmsg_lite_remote_init((void *)RPMSG_LITE_SHMEM_BASE, RPMSG_LITE_LINK_ID, RL_NO_FLAGS);
+
+    my_queue = rpmsg_queue_create(my_rpmsg);
+    my_ept   = rpmsg_lite_create_ept(my_rpmsg, LOCAL_EPT_ADDR, rpmsg_queue_rx_cb, my_queue);
+    (void)rpmsg_ns_announce(my_rpmsg, my_ept, RPMSG_LITE_NS_ANNOUNCE_STRING, RL_NS_CREATE);
+
+    printk("\r\nNameservice sent, ready for incoming messages...\r\n");
+
+    for (;;)
+    {
+        /* Get RPMsg rx buffer with message */
+        result =
+            rpmsg_queue_recv_nocopy(my_rpmsg, my_queue, (uint32_t *)&remote_addr, (char **)&rx_buf, &len, RL_BLOCK);
+        if (result != 0)
+        {
+            assert(false);
+        }
+
+        /* Copy string from RPMsg rx buffer */
+        assert(len < sizeof(app_buf));
+        memcpy(app_buf, rx_buf, len);
+        app_buf[len] = 0; /* End string by '\0' */
+
+        if ((len == 2) && (app_buf[0] == 0xd) && (app_buf[1] == 0xa))
+            printk("Get New Line From Master Side\r\n");
+        else
+            printk("Get Message From Master Side : \"%s\" [len : %d]\r\n", app_buf, len);
+
+        /* Get tx buffer from RPMsg */
+        tx_buf = rpmsg_lite_alloc_tx_buffer(my_rpmsg, &size, RL_BLOCK);
+        assert(tx_buf);
+        /* Copy string to RPMsg tx buffer */
+        memcpy(tx_buf, app_buf, len);
+        /* Echo back received message with nocopy send */
+        result = rpmsg_lite_send_nocopy(my_rpmsg, my_ept, remote_addr, tx_buf, len);
+        if (result != 0)
+        {
+            assert(false);
+        }
+        /* Release held RPMsg rx buffer */
+        result = rpmsg_queue_nocopy_free(my_rpmsg, rx_buf);
+        if (result != 0)
+        {
+            assert(false);
+        }
+    }
+#endif
+	return 0;
+
+#if 0
 	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
 	for(int i=0; i<10; i++)
 	{
@@ -20,6 +110,7 @@ int main(void)
 		printk("%d: Hello World! %s\n", i, CONFIG_ARCH);
 	}
 	return 0;
+#endif
 #if 0	
 	int ret;
 	const struct device *sensor;
